113cdedf5aaa5a2571485c19882ae94166c425cd
Lakshan Jayawardana <63670833+LakshanJayawardana@users.noreply.github.com>
2023-04-20 00:01:15 +0530

    Page structure setup

--- MovieStore/src/app/app.component.ts
+++ MovieStore/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MovieStore';
+  title = "MovieWeb";
+
+  movies = [{
+    title :'Spider Man',
+    releaseDate :Date(),
+    price : 1999.99
+  },
+  {
+    title :'3 Idiots',
+    releaseDate :Date(),
+    price : 1999.99
+  },
+  {
+    title :'Moana',
+    releaseDate :Date(),
+    price : 1999.99
+  }
+];
+
+
+  duplicateNumber(n: number){
+    return n*2;
+  }
 }
