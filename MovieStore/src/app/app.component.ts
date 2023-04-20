6217ad182850012361ec27e0c58374fffa605c73
Lakshan Jayawardana <63670833+LakshanJayawardana@users.noreply.github.com>
2023-04-20 16:22:41 +0530

    Movies-list component created

--- MovieStore/src/app/app.component.ts
+++ MovieStore/src/app/app.component.ts
@@ -1,32 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = "MovieWeb";
-
-  movies = [{
-    title :'Spider Man',
-    releaseDate :Date(),
-    price : 1999.99
-  },
-  {
-    title :'3 Idiots',
-    releaseDate :Date(),
-    price : 1999.99
-  },
-  {
-    title :'Moana',
-    releaseDate :Date(),
-    price : 1999.99
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.moviesInTheaters = [
+      {
+        title :'Spider Man',
+        releaseDate :Date(),
+        price : 1999.99
+      },
+      {
+        title :'3 Idiots',
+        releaseDate :Date(),
+        price : 1999.99
+      },
+      {
+        title :'Moana',
+        releaseDate :Date(),
+        price : 1999.99
+      }
+    ];
+    this.moviesFutureReleases = [
+      {
+        title :'Ice Men',
+        releaseDate :Date(),
+        price : 1999.99
+      },
+      {
+        title :'Iron Man',
+        releaseDate :Date(),
+        price : 1999.99
+      },
+      {
+        title :'Ice Age',
+        releaseDate :Date(),
+        price : 1999.99
+      }
+    ]
   }
-];
 
-
-  duplicateNumber(n: number){
-    return n*2;
-  }
+  moviesInTheaters : any[] = [];
+  moviesFutureReleases : any[] = [];
+  title = 'Movie app';
 }
+
