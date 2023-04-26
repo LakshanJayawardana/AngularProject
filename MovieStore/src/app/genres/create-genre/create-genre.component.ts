8cc0c62486cfe96ba89142ee0103f92a546e1978
Lakshan Jayawardana <63670833+LakshanJayawardana@users.noreply.github.com>
2023-04-27 00:57:31 +0530

    Added built in validation rules

--- MovieStore/src/app/genres/create-genre/create-genre.component.ts
+++ MovieStore/src/app/genres/create-genre/create-genre.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,12 +13,30 @@ export class CreateGenreComponent implements OnInit{
   form!: FormGroup;
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      name : 'Drama'
+      name : ['',{
+        validators: [Validators.required, Validators.minLength(3)]
+      }]
     })
   }
 
   saveChanges(){
     this.router.navigate(['/genres']);
+  }
+
+  getErrorMessageFieldName(){
+    const field = this.form.get('name');
+
+    if(field?.hasError('required')){
+      return 'The name field is required';
+    }
+
+    if(field?.hasError('minlength')){
+      return 'The minimum length is 3';
+    }
+    
+
+    return '';
+
   }
 
 }
