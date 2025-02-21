358370bdffc86db50a859f0d7be469f127de1e38
Lakshan Jayawardana <madusaralakshan@gmail.com>
2025-02-21 23:58:13 +0530

    Unwanted component removed

--- MovieStore/src/app/app-routing.module.ts
+++ MovieStore/src/app/app-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { IndexActorsComponent } from './actors/index-actors/index-actors.component';
-import { CreateActorComponent } from './actors/create-actor/create-actor.component';
-import { EditActorsComponent } from './actors/edit-actors/edit-actors.component';
 
 const routes: Routes = [
   {path : '', component:HomeComponent},
-
-
-
-  {path : 'actors', component:IndexActorsComponent},
-  {path : 'actors/create', component:CreateActorComponent},
-  {path : 'actors/edit/:id', component:EditActorsComponent},
-
   {path : '**', redirectTo: ''}
 
 
